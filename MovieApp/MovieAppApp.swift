8c252788f538c50f4ea7049d2c41ba02a6bdc1b9
zulalsarioglu <zulal.sariogluu@gmail.com>
2025-12-12 13:21:20 +0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Zülal Sarıoğlu on 12.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
